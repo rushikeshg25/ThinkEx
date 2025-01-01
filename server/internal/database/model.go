b302ca15ec4d7a9fcc615c4ed69d58c738abc0ce
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2025-01-02 01:04:41 +0530

    feat: db model

--- server/internal/database/model.go
+++ server/internal/database/model.go
@@ -3,18 +3,39 @@ package database
 import "time"
 
 type User struct {
-	Id        int       `gorm:"primaryKey;autoIncrement"`
+	Id        string    `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
 	Email     string    `gorm:"unique;not null"`
-	Role      UserRole
+	Role      UserRole	`gorm:"type:user_role;not null"`
 	Picture   string
-	Balance   int
+	Balance   float64
+	Events    []Event    `gorm:"many2many:event_participants"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
 
-type UserRole string
 
+type Event struct {
+	ID           string       `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	EventID      int          `gorm:"unique;autoIncrement"`
+	Description  string       `gorm:"not null"`
+	Title        string       `gorm:"not null"`
+	Start    	 time.Time    `gorm:"not null"`
+	End     	 time.Time    `gorm:"not null"`
+	Status       EventStatus  `gorm:"type:event_status;default:ONGOING"`
+	CreatedAt    time.Time    `gorm:"default:now()"`
+	UpdatedAt    time.Time    `gorm:"default:now()"`
+	Participants []User       `gorm:"many2many:event_participants"`
+}
+
+
+type UserRole string
 const (
 	ADMIN     UserRole = "admin"
 	CANDIDATE UserRole = "candidate"
+)
+
+type EventStatus string
+const (
+	Ongoing EventStatus = "ONGOING"
+	Ended   EventStatus = "ENDED"
 )
