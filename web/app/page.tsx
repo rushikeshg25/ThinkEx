e577a907752768d60454d93e90b2ced076b9296b
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2025-01-01 12:22:12 +0530

    feat: Dark theme and Navbar

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,11 @@
+"use client";
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      page
+      <a href="http://localhost:8080/api/auth/google">Login with Google</a>
+    </div>
+  );
 };
 
 export default page;
--- web/components/navbar.tsx
+++ web/components/navbar.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ModeToggle } from "./modetoggle";
 
-const navbar = () => {
-  return <div>navbar</div>;
+const Navbar = () => {
+  return (
+    <nav className="border-b">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16 items-center">
+          <div className="flex items-center">
+            <Link href="/" className="text-xl font-bold">
+              ThinkEx
+            </Link>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <Link href="/events">
+              <Button variant="ghost">Events</Button>
+            </Link>
+            <Link href="/portfolio">
+              <Button variant="ghost">Portfolio</Button>
+            </Link>
+
+            <ModeToggle />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
-export default navbar;
+export default Navbar;
